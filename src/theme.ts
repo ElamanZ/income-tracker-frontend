f005b4f64f2986904838ce858a18fcba2ca496ed
ElamanZ <z.elaman699@gmail.com>
2024-12-10 18:20:08 +0600

    fix font family

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-    fontFamily: `"Nunito Sans", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+    fontFamily: 'Inter, sans-serif',
     primaryColor: "green",
     components: {},
 });
